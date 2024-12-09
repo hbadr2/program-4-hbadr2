9ba4dacea0c36dd4559a19c22953588f2e744d0a
Hana <hanab@Hanas-MacBook-Air.local>
2024-12-09 02:09:33 -0500

    basic main file

--- program-4-hbadr2.git/Program.cs
+++ program-4-hbadr2.git/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#include "Server.h"
+#include "Client.h"
+#include <iostream>
 
+using namespace std;
+
+interface main() {
+    cout << "Thank you for contacting us! Someone will be with you shortly!" << endl;
+
+    Server server;
+    Client client;
+
+    //return 0;
+}
