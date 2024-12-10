ed31c3718bc729121fa668caebb34780e1819d79
hbadr2 <hbadr@mail.umw.edu>
2024-12-09 21:03:10 -0500

    fixed main

--- program-4-hbadr2.git/main.cpp
+++ program-4-hbadr2.git/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include "Server.h"
 #include "Client.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -13,10 +14,10 @@ int  main() {
     server.startServer(8080);
     client.connectToServer("127.0.0.1", 8080);
 
-    client.setUsername("John Smith");
+    client.setName("John Smith");
     client.sendMessage("Hello, everyone :)");
 
-    server.broadcastMessage("John Smith: ", 0j;
+    server.broadcastMessage("John Smith: Hello everyone :) ", 0);
 
     return 0;
 }
