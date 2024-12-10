b5d8ffe1ff1c6885f18a590b40f873bfed548fbb
hbadr2 <hbadr@mail.umw.edu>
2024-12-09 21:02:36 -0500

    added server classes

---
+++ program-4-hbadr2.git/Server.cpp
@@ -0,0 +1,44 @@
+#include "Server.h"
+int s;
+
+Server::Server() {
+    s = -1;
+}
+
+Server::~Server() {
+    stopServer();
+}
+
+int Server::getServer() {
+    int s;
+    return s;
+}
+
+void Server::setServer(int s) {
+    server = s;
+}
+
+bool Server::startServer(int port) {
+    cout << "Server started on port " << port << endl;
+    return true;
+}
+
+void Server::stopServer() {
+    cout << "Server stopped." << endl;
+}
+
+void Server::run() {
+    cout << "Server is running ..." << endl;
+}
+
+void Server::acceptConnections() {
+    //something
+}
+
+void Server::broadcastMessage(const string& message, int s) {
+    //something
+}
+
+void Server::handleDisconnection(int s) {
+    //something
+}
---
+++ program-4-hbadr2.git/Server.h
@@ -0,0 +1,41 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <vector>
+#include <string>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+class Server {
+private:
+    int server;
+    vector<int> clientsAwaiting;
+    map <int, string> clientNames;
+
+public:
+    
+    //Constructor
+   Server();
+   ~Server();
+
+    int getServer();
+
+    void setServer(int s);
+
+    bool startServer(int port);
+
+    void stopServer();
+
+    void run();
+
+    void acceptConnections();
+
+    void broadcastMessage(const string& message, int s);
+
+    void handleDisconnection(int s);
+}
+;
+
+#endif
